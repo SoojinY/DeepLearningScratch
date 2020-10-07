b74d5a827fe515e1d4b587e5ed86588b822c211f
SoojinY <2018112174@dgu.ac.kr>
2020-10-07 19:41:16 +0900

    'init'

---
+++ venv/Scripts/rst2odt.py
@@ -0,0 +1,30 @@
+#!c:\users\pc\desktop\스터디\딥러닝\딥러닝 예제\딥러닝 파이챰\venv\scripts\python.exe
+
+# $Id: rst2odt.py 5839 2009-01-07 19:09:28Z dkuhlman $
+# Author: Dave Kuhlman <dkuhlman@rexx.com>
+# Copyright: This module has been placed in the public domain.
+
+"""
+A front end to the Docutils Publisher, producing OpenOffice documents.
+"""
+
+import sys
+try:
+    import locale
+    locale.setlocale(locale.LC_ALL, '')
+except:
+    pass
+
+from docutils.core import publish_cmdline_to_binary, default_description
+from docutils.writers.odf_odt import Writer, Reader
+
+
+description = ('Generates OpenDocument/OpenOffice/ODF documents from '
+               'standalone reStructuredText sources.  ' + default_description)
+
+
+writer = Writer()
+reader = Reader()
+output = publish_cmdline_to_binary(reader=reader, writer=writer,
+    description=description)
+
---
+++ venv/Scripts/rst2xml.py
@@ -0,0 +1,23 @@
+#!c:\users\pc\desktop\스터디\딥러닝\딥러닝 예제\딥러닝 파이챰\venv\scripts\python.exe
+
+# $Id: rst2xml.py 4564 2006-05-21 20:44:42Z wiemann $
+# Author: David Goodger <goodger@python.org>
+# Copyright: This module has been placed in the public domain.
+
+"""
+A minimal front end to the Docutils Publisher, producing Docutils XML.
+"""
+
+try:
+    import locale
+    locale.setlocale(locale.LC_ALL, '')
+except:
+    pass
+
+from docutils.core import publish_cmdline, default_description
+
+
+description = ('Generates Docutils-native XML from standalone '
+               'reStructuredText sources.  ' + default_description)
+
+publish_cmdline(writer_name='xml', description=description)
